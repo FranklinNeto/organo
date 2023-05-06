fe38a0e0f8c3227851fdd9740dcedee532fe0f85
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-05-06 03:21:14 -0400

    feat: add Time component

---
+++ src/components/Time/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const Time = (props) => {
+  const estilos = { backgroundColor: props.corSecundaria };
+  return (
+    <section className="time" style={estilos}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+    </section>
+  );
+};
+
+export default Time;
